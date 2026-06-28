58e42888293802fd3b91b6abb2f1484df6adcc20
biloa2005 <biloaphilemon@gmail.com>
2026-06-28 02:42:30 +0200

    rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,6 +21,8 @@ import { ProduceDebtModule } from './produce-debt/produce-debt.module';
 import { MydebtModule } from './mydebt/mydebt.module';
 import { AiModule } from './ai/ai.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 
 
@@ -28,7 +30,12 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+     {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   imports: [
     ConfigModule.forRoot({
       isGlobal:true
@@ -58,9 +65,18 @@ import { ConfigModule } from '@nestjs/config';
     ProduceDebtModule,
     MydebtModule,
     AiModule,
+    ThrottlerModule.forRoot({
+      throttlers:[
+        {
+          ttl:60000,
+          limit:10
+        }
+      ]
+      })
     
     
   ],
+
 
 
 })
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,17 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:['http://localhost:3000',
+    'http://192.168.56.1:3000',]
+,    credentials:true,
+  })
    app.useGlobalPipes(new ValidationPipe({
     transform:true,
    }));
-   app.use(helmet());
-  await app.listen(process.env.PORT ?? 3000);
+   app.use(helmet({
+    
+   }));
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
